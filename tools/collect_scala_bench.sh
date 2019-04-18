9931a59e53f3d8bf56502dba5c94f23f31712222
Lukáš <33589083+rqu@users.noreply.github.com>
2019-04-18 03:40:49 +0200

    Updated `tools/collect_scala_bench.sh`.

--- tools/collect_scala_bench.sh
+++ tools/collect_scala_bench.sh
@@ -1,25 +1,26 @@
-#!/bin/bash
+#!/bin/bash -e
 
 # This script runs instrumented Graal on all ScalaBench benchmarks
 # saving all the dumps into dumps/ top-level directory. As a first
 # argument to this script, path to ScalaBench jar file is required.
 
 
-#FIXME validate arguments
 #FIXME the benchmark does not seem to stop by itself.
 #FIXME the benchmarks don't like running in our instrumented Graal
 #      and they keep crashing. Can we do something with that?
+
+if [[ $# != 1 ]]; then
+	echo "Usage: $0 SCALABENCH.JAR" >&2
+	exit 1
+fi
 
 project_root=$(git rev-parse --show-toplevel)
 scalabench_jar="$(pwd)/$1"
 benchmarks=$(java -jar "$scalabench_jar" --list-benchmarks)
 
 cd "$project_root"
-mkdir -p "dumps"
 
 for bench in $benchmarks; do
-	name="${bench}_depmat_$(date "+%Y-%m-%d_%H-%M-%S")"
 	./vm -jar "$scalabench_jar" "$bench"
-	mv /tmp/gcopdd-depmat dumps/"$name"
 done
 
