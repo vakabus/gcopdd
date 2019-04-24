ae9050703d7fd9c0058e2895287efb49e2cba192
Lukáš <33589083+rqu@users.noreply.github.com>
2019-04-24 11:29:27 +0200

    Added crGetMethod and getSignatureOfMethod utility methods for dealing with CompilationRequests.

--- blood/src/main/java/cz/cuni/mff/d3s/blood/utils/Miscellaneous.java
+++ blood/src/main/java/cz/cuni/mff/d3s/blood/utils/Miscellaneous.java
@@ -42,4 +42,33 @@ public final class Miscellaneous {
         // restore the previous state of java.lang.reflect.Field, whatever it was
         modifiersField.setAccessible(oldState);
     }
+
+    /**
+     * Calls CompilationRequest.getMethod() and returns its result.
+     *
+     * @param compilationRequest instance of jdk.vm.ci.code.CompilationRequest
+     * @return instance of jdk.vm.ci.meta.ResolvedJavaMethod
+     */
+    private static Object crGetMethod(Object compilationRequest) {
+        try {
+            return compilationRequest.getClass().getMethod("getMethod").invoke(compilationRequest);
+        } catch (ReflectiveOperationException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    /**
+     * Gets the signature of a jdk.vm.ci.meta.ResolvedJavaMethod.
+     *
+     * @param method instance of jdk.vm.ci.meta.ResolvedJavaMethod
+     * @return for example
+     * "CompilationContextTracker.getSignatureOfMethod(Object)"
+     */
+    private static String getSignatureOfMethod(Object method) {
+        String s = method.toString();
+        // so far, I have only seen "HotSpotMethod<...>", but let's be general
+        int left = s.indexOf('<');
+        int right = s.lastIndexOf('>');
+        return s.substring(left + 1, right == -1 ? s.length() : right);
+    }
 }
