1632e204a59c8fb4480233f20eec2be2d07adb9a
Vasek Sraier <git@vakabus.cz>
2019-04-27 19:34:57 +0200

    added phase number tracker

---
+++ blood/src/main/java/cz/cuni/mff/d3s/blood/instrumentations/PhaseOrderTracker.java
@@ -0,0 +1,4 @@
+package cz.cuni.mff.d3s.blood.instrumentations;
+
+public class PhaseOrderTracker {
+}
---
+++ blood/src/main/java/cz/cuni/mff/d3s/blood/phase_order_tracker/IncrementCounterPhase.java
@@ -0,0 +1,4 @@
+package cz.cuni.mff.d3s.blood.phase_order_tracker;
+
+public class IncrementCounterPhase {
+}
