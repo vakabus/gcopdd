6998b01ea13fea862d33413d04a1e1ac19fe61bb
Lukáš <33589083+rqu@users.noreply.github.com>
2019-05-07 01:33:46 +0200

    Auto-scaling colors in node type matrix.

--- tools/viewers/nodetypematrix.py
+++ tools/viewers/nodetypematrix.py
@@ -21,16 +21,23 @@ def pretty_number(num):
 		return str(num)
 
 
-def html_td(dv, row_desc, col_desc):
-	hue = int((1-dv.ratio()**.25) * 120) # TODO dynamic scaling
-	sat = 100 # TODO
+def html_td(dv, row_desc, col_desc, col_max):
+	hue = int((1 - dv.ratio()/(col_max or 1)) * 120)
+	sat = min(dv.iterations, 100)
 	lit = 50 # 0%..100% ~ black..white
 	color = 'hsl(%i, %i%%, %i%%)' % (hue, sat, lit)
 	td = '<td style="background-color: %s" title="%s\nNode %s\nPhase %s">%i%%<br>%s</td>' % (color, dv, col_desc.simplename, row_desc.simplename, dv.ratio()*100, pretty_number(dv.iterations))
 	yield td
 
 
-def html_table(col_classes, row_classes, matrix):
+def find_column_maxima(matrix):
+	# convert matrix from array of rows to array of columns
+	# for each column: calculate all the ratio()s and find the highest of them
+	# return array, where each element corresponds to a row
+	return [max(map(DependencyValue.ratio, column)) for column in zip(*matrix)]
+
+
+def html_table(col_classes, row_classes, matrix, col_maxs):
 	yield '<table class="simple-border">'
 	yield '<tr>'
 	yield '<th>&nbsp;</th>'
@@ -40,8 +47,8 @@ def html_table(col_classes, row_classes, matrix):
 	for row_desc, row in zip(row_classes, matrix):
 		yield '<tr>'
 		yield '<th>%i</th>' % (row_desc.index)
-		for col_desc, dv in zip(col_classes, row):
-			yield from html_td(dv, row_desc, col_desc)
+		for col_desc, dv, col_max in zip(col_classes, row, col_maxs):
+			yield from html_td(dv, row_desc, col_desc, col_max)
 		yield '</tr>'
 	yield '</table>'
 
@@ -76,8 +83,8 @@ def view(lines_n, dump, params):
 	post_phase_matrix = [[DependencyValue(*map(int, item.split(':'))) for item in line.split(' ')] for line in takewhile(bool, lines)]
 
 	yield '<table style="font-family: monospace"><tr><td>'
-	yield from html_table(nodeClasses, phaseClasses, pre_phase_matrix)
-	yield from html_table(nodeClasses, phaseClasses, post_phase_matrix)
+	yield from html_table(nodeClasses, phaseClasses, pre_phase_matrix, find_column_maxima(pre_phase_matrix))
+	yield from html_table(nodeClasses, phaseClasses, post_phase_matrix, find_column_maxima(post_phase_matrix))
 	yield '</td><td style="text-align: left; vertical-align: top">'
 	yield from html_legend(nodeClasses)
 	yield from html_legend(phaseClasses)
