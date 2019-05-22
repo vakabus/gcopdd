70895a3179073138d166a771fbb0ce60bbdca67e
Vasek Sraier <git@vakabus.cz>
2019-05-22 21:58:46 +0200

    dependency matrix and node type tracker dumping split per method

--- blood/src/main/java/cz/cuni/mff/d3s/blood/node_origin_tracker/DependencyMatrixCollector.java
+++ blood/src/main/java/cz/cuni/mff/d3s/blood/node_origin_tracker/DependencyMatrixCollector.java
@@ -1,5 +1,6 @@
 package cz.cuni.mff.d3s.blood.node_origin_tracker;
 
+import cz.cuni.mff.d3s.blood.method_local.CompilationEventLocal;
 import cz.cuni.mff.d3s.blood.report.Report;
 import cz.cuni.mff.d3s.blood.report.dump.ManualTextDump;
 import cz.cuni.mff.d3s.blood.utils.ConcurrentMatrix;
@@ -7,41 +8,16 @@ import cz.cuni.mff.d3s.blood.utils.ConcurrentOrderedSet;
 import org.graalvm.compiler.graph.Node;
 import org.graalvm.compiler.nodes.StructuredGraph;
 
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReadWriteLock;
-import java.util.concurrent.locks.ReentrantReadWriteLock;
 import java.util.stream.Collectors;
 
 public final class DependencyMatrixCollector {
-
     // the default of 16 doesn't fit even the most trivial programs
     private static final int HASHMAP_INIT_CAPACITY = 64;
-
     private static DependencyMatrixCollector instance = null;
-    /**
-     * Multiple threads are writing to the result matrix at once. That's fine.
-     * However, in the end, we want to dump the data and nobody should be
-     * writing at that time. We can achieve this kind of locking by using
-     * {@link ReadWriteLock} in the opposite way, than it was designed. Lock for
-     * reading, when we are writing. Lock for writing, when we are reading.
-     */
-    private final ReadWriteLock writers = new ReentrantReadWriteLock(true);
-
-    private final ConcurrentOrderedSet<PhaseID> phaseOrder = new ConcurrentOrderedSet<>();
-    private final ConcurrentMatrix<PhaseID, PhaseID, DependencyValue> matrix = new ConcurrentMatrix<>(HASHMAP_INIT_CAPACITY, DependencyValue.ZERO);
-
     // TODO make this configurable, also works with `new DefaultNodeTracker()`
     private final NodeTracker nodeTracker = new CustomNodeTracker();
 
-    {
-        phaseOrder.add(NodeTracker.NO_PHASE_DUMMY_PHASE_ID);
-        phaseOrder.add(NodeTracker.DELETED_PHASE_DUMMY_PHASE_ID);
-    }
-
-    private DependencyMatrixCollector() {
-        // register hook for dumping data
-        Report.getInstance().registerDump(new ManualTextDump("depmat", this::dump));
-    }
+    private CompilationEventLocal<DependencyMatrix> matrix = new CompilationEventLocal<>(() -> new DependencyMatrix(), dependencyMatrix -> Report.getInstance().dumpNow(new ManualTextDump("depmat", dependencyMatrix::dump)));
 
     public static DependencyMatrixCollector getInstance() {
         if (instance == null) {
@@ -68,49 +44,11 @@ public final class DependencyMatrixCollector {
      * phase run. More specifically, before calling
      * {@link org.graalvm.compiler.phases.BasePhase#apply(StructuredGraph, Object)}
      *
-     * @param graph Graph entering the optimization phase
+     * @param graph       Graph entering the optimization phase
      * @param sourceClass Class of the optimization phase running
      */
     public final void prePhase(StructuredGraph graph, Class<?> sourceClass) {
-        Lock readLock = writers.readLock();
-        if (!readLock.tryLock()) {
-            // Dump is already in progress, don't change the matrix any more.
-            return;
-        }
-
-        var phaseID = getCurrentPhaseId(sourceClass);
-
-        try {
-            phaseOrder.add(phaseID);
-
-            // obtain row in result matrix for this particular optimization phase
-            var row = matrix.getOrCreateRow(phaseID);
-
-            // for each node in the graph entering the phase, note down where it was created
-            for (Node node : graph.getNodes()) {
-                var creationPhaseResult = nodeTracker.getCreationPhase(node);
-
-                if (creationPhaseResult.isError()) {
-                    System.err.println(creationPhaseResult.unwrapError());
-                    continue;
-                }
-
-                var creationPhase = creationPhaseResult.unwrap();
-
-                DependencyValue value = row.getOrCreate(creationPhase);
-                value.incrementNumberOfSeenNodes(1);
-            }
-
-            // update total node counts for all tracked values
-            row.valuesStream().forEach(value -> {
-                value.incrementTotalNumberOfNodesSeen(graph.getNodeCount());
-                value.incrementPhaseCounter();
-            });
-
-        } finally {
-            // don't forget to unlock the lock
-            readLock.unlock();
-        }
+        matrix.get().update(graph, sourceClass);
     }
 
     /**
@@ -118,8 +56,8 @@ public final class DependencyMatrixCollector {
      * phase run. More specifically, after calling
      * {@link org.graalvm.compiler.phases.BasePhase#apply(StructuredGraph, Object)}
      *
-     * @param graph Graph representing IL after being processed by the
-     * optimization phase
+     * @param graph       Graph representing IL after being processed by the
+     *                    optimization phase
      * @param sourceClass Class of the running optimization phase
      */
     public final void postPhase(StructuredGraph graph, Class<?> sourceClass) {
@@ -131,23 +69,51 @@ public final class DependencyMatrixCollector {
         return new PhaseID(sourceClass);
     }
 
-    /**
-     * Method called on JVM exit dumping collected statistics.
-     */
-    private String dump() {
-        // Block, so that nobody can write to the matrix.
-        Lock writeLock = writers.writeLock();
-        writeLock.lock();
-        try {
+    final class DependencyMatrix {
+        private final ConcurrentOrderedSet<Class> phaseOrder = new ConcurrentOrderedSet<>();
+        private final ConcurrentMatrix<Class, Class, DependencyValue> matrix = new ConcurrentMatrix<>(HASHMAP_INIT_CAPACITY, DependencyValue.ZERO);
+
+        public DependencyMatrix() {
+            phaseOrder.add(NodeTracker.NoPhaseDummy.class);
+            phaseOrder.add(NodeTracker.DeletedPhaseDummy.class);
+        }
+
+        public String dump() {
             String header = phaseOrder.stream()
-                    .map(PhaseID::toString)
+                    .map(Class::getName)
                     .collect(Collectors.joining("\n"));
 
             String data = matrix.toString(phaseOrder::stream, phaseOrder::stream);
 
             return header + "\n\n" + data;
-        } finally {
-            writeLock.unlock();
+        }
+
+        public void update(StructuredGraph graph, Class sourceClass) {
+            phaseOrder.add(sourceClass);
+
+            // obtain row in result matrix for this particular optimization phase
+            var row = matrix.getOrCreateRow(sourceClass);
+
+            // for each node in the graph entering the phase, note down where it was created
+            for (Node node : graph.getNodes()) {
+                var creationPhaseResult = nodeTracker.getCreationPhase(node);
+
+                if (creationPhaseResult.isError()) {
+                    System.err.println(creationPhaseResult.unwrapError());
+                    continue;
+                }
+
+                var creationPhaseClass = creationPhaseResult.unwrap().phaseClass;
+
+                DependencyValue value = row.getOrCreate(creationPhaseClass);
+                value.incrementNumberOfSeenNodes(1);
+            }
+
+            // update total node counts for all tracked values
+            row.valuesStream().forEach(value -> {
+                value.incrementTotalNumberOfNodesSeen(graph.getNodeCount());
+                value.incrementPhaseCounter();
+            });
         }
     }
 }
--- blood/src/main/java/cz/cuni/mff/d3s/blood/node_type_tracker/NodeTypeTrackerCollector.java
+++ blood/src/main/java/cz/cuni/mff/d3s/blood/node_type_tracker/NodeTypeTrackerCollector.java
@@ -1,5 +1,6 @@
 package cz.cuni.mff.d3s.blood.node_type_tracker;
 
+import cz.cuni.mff.d3s.blood.method_local.CompilationEventLocal;
 import cz.cuni.mff.d3s.blood.report.Report;
 import cz.cuni.mff.d3s.blood.report.dump.ManualTextDump;
 import cz.cuni.mff.d3s.blood.utils.ConcurrentMatrix;
@@ -9,28 +10,14 @@ import org.graalvm.compiler.nodes.StructuredGraph;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReadWriteLock;
-import java.util.concurrent.locks.ReentrantReadWriteLock;
 import java.util.stream.Collectors;
 
 public class NodeTypeTrackerCollector {
-
-    // the default of 16 doesn't fit even the most trivial programs
-    private static final int HASHMAP_INIT_CAPACITY = 64;
-
     private static NodeTypeTrackerCollector instance = null;
 
-    private final ReadWriteLock writers = new ReentrantReadWriteLock(true);
-
-    private final ConcurrentOrderedSet<Class> nodeClasses = new ConcurrentOrderedSet<>();
-    private final ConcurrentOrderedSet<Class> phaseClasses = new ConcurrentOrderedSet<>();
-
-    private final ConcurrentMatrix<Class, Class, NodeTrackerValue> prePhaseTypes = new ConcurrentMatrix<>(HASHMAP_INIT_CAPACITY, NodeTrackerValue.ZERO);
-    private final ConcurrentMatrix<Class, Class, NodeTrackerValue> postPhaseTypes = new ConcurrentMatrix<>(HASHMAP_INIT_CAPACITY, NodeTrackerValue.ZERO);
+    private CompilationEventLocal<NodeTypeMatrix> matrix = new CompilationEventLocal<>(() -> new NodeTypeMatrix(), nodeTypeMatrix -> Report.getInstance().dumpNow(new ManualTextDump("nodetypematrix", nodeTypeMatrix::dump)));
 
     private NodeTypeTrackerCollector() {
-        Report.getInstance().registerDump(new ManualTextDump("nodetypematrix", this::dump));
     }
 
     public static NodeTypeTrackerCollector getInstance() {
@@ -44,40 +31,17 @@ public class NodeTypeTrackerCollector {
         return instance;
     }
 
-    public final void updateRow(StructuredGraph graph, ConcurrentMatrix<Class, Class, NodeTrackerValue>.Row row) {
-        HashMap<Class, Long> nodeCount = new HashMap<>();
-        for (Node n : graph.getNodes()) {
-            Long count = nodeCount.getOrDefault(n.getClass(), 0l);
-            count++;
-            nodeCount.put(n.getClass(), count);
-        }
-
-        for (Map.Entry<Class, Long> classLongEntry : nodeCount.entrySet()) {
-            Class nodeClass = classLongEntry.getKey();
-
-            nodeClasses.add(nodeClass);
-
-            row.getOrCreate(nodeClass)
-                    .incrementNumberOfSeenNodes(classLongEntry.getValue());
-        }
-
-        final long totalCount = graph.getNodeCount();
-        row.valuesStream().forEach(nodeTrackerValue -> nodeTrackerValue.incrementTotalNumberOfNodesSeen(totalCount));
-        row.valuesStream().forEach(NodeTrackerValue::incrementPhaseCounter);
-    }
 
     /**
      * This function is called by the instrumentation before every optimization
      * phase run. More specifically, before calling
      * {@link org.graalvm.compiler.phases.BasePhase#apply(StructuredGraph, Object)}
      *
-     * @param graph Graph entering the optimization phase
+     * @param graph       Graph entering the optimization phase
      * @param sourceClass Class of the optimization phase running
      */
     public final void prePhase(StructuredGraph graph, Class<?> sourceClass) {
-        var row = prePhaseTypes.getOrCreateRow(sourceClass);
-        phaseClasses.add(sourceClass);
-        updateRow(graph, row);
+        matrix.get().updatePre(graph, sourceClass);
     }
 
     /**
@@ -85,23 +49,62 @@ public class NodeTypeTrackerCollector {
      * phase run. More specifically, after calling
      * {@link org.graalvm.compiler.phases.BasePhase#apply(StructuredGraph, Object)}
      *
-     * @param graph Graph representing IL after being processed by the
-     * optimization phase
+     * @param graph       Graph representing IL after being processed by the
+     *                    optimization phase
      * @param sourceClass Class of the running optimization phase
      */
     public final void postPhase(StructuredGraph graph, Class<?> sourceClass) {
-        var row = postPhaseTypes.getOrCreateRow(sourceClass);
-        updateRow(graph, row);
+        matrix.get().updatePost(graph, sourceClass);
     }
 
-    /**
-     * Method called on JVM exit dumping collected statistics.
-     */
-    private String dump() {
-        // Block, so that nobody can write to the matrix.
-        Lock writeLock = writers.writeLock();
-        writeLock.lock();
-        try {
+
+    static final class NodeTypeMatrix {
+        // the default of 16 doesn't fit even the most trivial programs
+        private static final int HASHMAP_INIT_CAPACITY = 64;
+
+        private ConcurrentMatrix<Class, Class, NodeTrackerValue> preMatrix = new ConcurrentMatrix<>(HASHMAP_INIT_CAPACITY, NodeTrackerValue.ZERO);
+        private ConcurrentMatrix<Class, Class, NodeTrackerValue> postMatrix = new ConcurrentMatrix<>(HASHMAP_INIT_CAPACITY, NodeTrackerValue.ZERO);
+
+        private ConcurrentOrderedSet<Class> nodeClasses = new ConcurrentOrderedSet<>();
+        private ConcurrentOrderedSet<Class> phaseClasses = new ConcurrentOrderedSet<>();
+
+        public final void updatePre(StructuredGraph graph, Class<?> phaseClass) {
+            phaseClasses.add(phaseClass);
+
+            var row = preMatrix.getOrCreateRow(phaseClass);
+            this.updateRow(graph, row);
+        }
+
+        public final void updatePost(StructuredGraph graph, Class<?> phaseClass) {
+            phaseClasses.add(phaseClass);
+
+            var row = postMatrix.getOrCreateRow(phaseClass);
+            this.updateRow(graph, row);
+        }
+
+        public final void updateRow(StructuredGraph graph, ConcurrentMatrix<Class, Class, NodeTrackerValue>.Row row) {
+            HashMap<Class, Long> nodeCount = new HashMap<>();
+            for (Node n : graph.getNodes()) {
+                Long count = nodeCount.getOrDefault(n.getClass(), 0l);
+                count++;
+                nodeCount.put(n.getClass(), count);
+            }
+
+            for (Map.Entry<Class, Long> classLongEntry : nodeCount.entrySet()) {
+                Class nodeClass = classLongEntry.getKey();
+
+                nodeClasses.add(nodeClass);
+
+                row.getOrCreate(nodeClass)
+                        .incrementNumberOfSeenNodes(classLongEntry.getValue());
+            }
+
+            final long totalCount = graph.getNodeCount();
+            row.valuesStream().forEach(nodeTrackerValue -> nodeTrackerValue.incrementTotalNumberOfNodesSeen(totalCount));
+            row.valuesStream().forEach(NodeTrackerValue::incrementPhaseCounter);
+        }
+
+        public final String dump() {
             String nodeClassesStr = nodeClasses.stream()
                     .map(Class::getName)
                     .collect(Collectors.joining("\n"));
@@ -110,13 +113,10 @@ public class NodeTypeTrackerCollector {
                     .map(Class::getName)
                     .collect(Collectors.joining("\n"));
 
-            String prePhaseStr = prePhaseTypes.toString(phaseClasses::stream, nodeClasses::stream);
-
-            String postPhaseStr = postPhaseTypes.toString(phaseClasses::stream, nodeClasses::stream);
+            String prePhaseStr = preMatrix.toString(phaseClasses::stream, nodeClasses::stream);
+            String postPhaseStr = postMatrix.toString(phaseClasses::stream, nodeClasses::stream);
 
             return nodeClassesStr + "\n\n" + phaseClassesStr + "\n\n" + prePhaseStr + "\n\n" + postPhaseStr;
-        } finally {
-            writeLock.unlock();
         }
     }
 }
